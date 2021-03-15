f97f247616eb7a64a77c282415e938a59d90c36c
az9702km <az9798mt@gmail.com>
2021-03-15 21:29:27 +0500

    fix: solve problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
+  if(arguments.length && array.length !== 0){
+    return Math.min.apply(null, array);
+  } else
   return 0;
 }
 
 exports.max = function max (array) {
+  if(arguments.length && array.length !== 0){
+    return Math.max.apply(null, array);
+  } else
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if(arguments.length && array.length !== 0){
+    const sum = array.reduce((a, b) => a + b, 0);
+    const avg = (sum / array.length) || 0;
+    return avg;
+  } else
   return 0;
 }
